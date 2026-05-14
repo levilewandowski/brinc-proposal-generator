2e0f8097596bca0a212ef800d8c6f000d443a034
Levi Lewandowski <levi.lewandowski@brinc.io>
2026-05-14 16:38:44 +0800

    Flatten API files with google/ wrappers for backward compat

--- api/google/callback.ts
+++ api/google/callback.ts
@@ -1,7 +1,2 @@
-export default function handler(req: any, res: any) {
-  const code = req.query.code;
-  const error = req.query.error;
-  if (error) return res.redirect(`/?google_error=${encodeURIComponent(error)}`);
-  if (!code) return res.redirect("/?google_error=no_code");
-  return res.redirect(`/?google_code=${encodeURIComponent(code)}`);
-}
+import handler from "../callback";
+export default handler;
